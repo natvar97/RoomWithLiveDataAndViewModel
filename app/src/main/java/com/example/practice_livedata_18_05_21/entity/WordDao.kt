8d9e3c079afbcf52b2a28225d60de86dcb1b88f8
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-05-18 14:41:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practice_livedata_18_05_21/entity/WordDao.kt
@@ -0,0 +1,20 @@
+package com.example.practice_livedata_18_05_21.entity
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAllWords(): List<Word>
+
+}
